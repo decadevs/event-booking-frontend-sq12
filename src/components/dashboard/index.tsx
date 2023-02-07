60f0760d1673afc2e61004c640521cc38ecb93ca
kcblack22@gmail.com <kcblack22@gmail.com>
2023-02-07 11:56:36 +0100

    Customized the theme to our color

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,24 +1,20 @@
 import React, {useContext} from "react"
-import styled from 'styled-components';
+import {styled} from '@mui/system'
+import {Box} from '@mui/material'
 
-
-
-const Container = styled.div`
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    color: var(--white);
-    justify-content: space-between;
-    background-color: var(--primary);
-    padding: 10px
-
-`
+const Container = styled('div')(({ theme }) => ({
+    color: theme.palette.secondary.main,
+    backgroundColor: theme.palette.primary.main,
+    padding: theme.spacing(1),
+    borderRadius: theme.shape.borderRadius,
+  }));
 
 export const Dasboard: React.FC = () => {
    const user = import.meta.env.VITE_SOME_KEY
 return (
     <Container>
         <h2> Hello  {user} </h2>
+        <Box sx={{color:(theme) => theme.palette.secondary.main }}> Hello People </Box>
         <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa facere id omnis fuga, 
             asperiores neque nemo accusamus? Odit, voluptatum dignissimos.</p>
     </Container>
--- src/utils/theme/index.ts
+++ src/utils/theme/index.ts
@@ -1,43 +1,22 @@
-import { createTheme } from '@material-ui/core/styles'
-const PRIMARY_COLOR = '#10375C';
-const SECONDARY_COLOR = '#FF5722';
-const SECONDARY_COLOR_LIGHT = "rgba(255, 87, 34, 0.1)";
-const FONT_SM = 13;
-export const theme = createTheme({
-	typography: {
-		fontFamily: 'Open Sans, sans-serif',
-		fontSize: 16,
-		button: { textTransform: 'none', letterSpacing: 0, fontWeight: 'bold' },
+import { Colors } from "./Colors";
+
+export const Bookerpallette = {
+	primary: {
+		main: Colors.primary_main,
+		dark: Colors.primary_dark
 	},
-	overrides: {
-		MuiFormControl: { root: { marginBottom: '15px' } },
-		MuiInput: {
-			input: { fontWeight: 'bold' },
-			underline: {
-				'&:before': { borderBottom: '1px solid #d5dfee' },
-				'&:after': { borderBottom: `3px solid ${PRIMARY_COLOR}` },
-				'&:hover:not($disabled):not($focused):not($error):before': {
-					borderBottom: `1px solid ${PRIMARY_COLOR}`,
-				},
-			},
-		},
-		MuiFormLabel: {
-			root: {
-				color: SECONDARY_COLOR,
-				fontSize: '18px',
-				paddingLeft: '5px',
-				'&$focused': { paddingLeft: '5px', color: SECONDARY_COLOR },
-			},
-			asterisk: { color: 'transparent' },
-		},
-		MuiTypography: { h4: { fontSize: '2rem', fontWeight: 'bold' } },
-	},
-	palette: {
-		primary: { main: PRIMARY_COLOR },
-		secondary: { main: SECONDARY_COLOR },
-		gradient: {
-			start: (opacity) => `rgb(58 141 255 / ${opacity || 100}%)`,
-			end: (opacity) => `rgb(134 185 255 / ${opacity || 100}%)`,
-		},
-	},
-});
+	secondary: {
+		main:Colors.secondary_main,
+		light: Colors.secondary_light
+	}
+}
+
+
+export const appTheme = {
+    
+    shape: {
+        searchbar:{
+            borderRadius: 30
+        }
+    }
+}
