36e78c8e2bd9ad5da693366e31aaf8235df11e07
imaosazee4github <86365081+imaosazee4github@users.noreply.github.com>
2023-02-09 09:26:26 +0100

    second commit on ticketsummary 9th feb

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import { useState , createContext} from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { DemoPage } from './pages/demo'
 import { DashboardPage } from './pages/dashboard/DashboardPage';
-import { BookrProvider } from './utils/context';
+import { ContextProvider } from './utils/context';
+import { Bookerpallette } from './utils/theme';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    ...Bookerpallette
+  },
+})
 
 
 function App() {
   return (
     <>
-    <BookrProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='' element={<DemoPage />} />
-          <Route path='/dashboard' element={<DashboardPage />} />
-        </Routes>
-      </BrowserRouter>
-
-    </BookrProvider>
+      <ContextProvider>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Routes>
+              <Route path='' element={<DemoPage />} />
+              <Route path='/dashboard' element={<DashboardPage />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </ContextProvider>
     </>
-
   )
 }
 
