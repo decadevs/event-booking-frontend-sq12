d3aa92beab552a0f3545c535b56c573d24850715
Chibuike Ilo <82292997+WESTIEWEB@users.noreply.github.com>
2023-02-15 13:09:20 +0100

    payment and account information page merged to dev

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-// import TextField from "@material-ui/TextField";import React from "react";
-=======
 // import TextField from "@material-ui/TextField";
 import React from "react";
->>>>>>> dev
 import FormControl from '@mui/material/FormControl';
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -11,37 +7,16 @@ interface Props {
     name: string;
     type: "text" | "number" | "email" | "password" | "date";
     [key: string]: any;
-<<<<<<< HEAD
-    label: string    
-    value: string    
-    id: string    
-    placeholder?: string    
-=======
     label: string
     value: string
     id: string
     placeholder?: string
->>>>>>> dev
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
     boxSx?: Record<string, unknown>;
 }
 const InputField: React.FC<Props> = ({ type, label, onChange, value, name, id, placeholder, boxSx, ...props }) => {
     return (
         <Box sx={boxSx}>
-<<<<<<< HEAD
-            <FormControl fullWidth>                
-                <TextField                    
-                label={label}
-                id={id}                    
-                type={type}
-                value={value}
-                name={name}
-                placeholder={placeholder}
-                onChange={onChange}
-                />            
-            </FormControl>        
-        </Box>     
-=======
             <FormControl fullWidth>
                 <TextField
                     label={label}
@@ -54,7 +29,6 @@ const InputField: React.FC<Props> = ({ type, label, onChange, value, name, id, p
                 />
             </FormControl>
         </Box>
->>>>>>> dev
     )
 };
 export default InputField;
