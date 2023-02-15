64ba6953cfca726e570f282c67e29ed6e29fca0e
Erhimuebru <erhimu.ebru@decagon.dev>
2023-02-15 12:16:29 +0100

    eventDetailsPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,46 @@
-import './App.css'
-import Footer from './components/footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { DemoPage } from './pages/demo'
-import { DashboardPage } from './pages/dashboard/DashboardPage';
+import "./App.css";
+import Footer from "./components/footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { DemoPage } from "./pages/demo";
+import { DashboardPage } from "./pages/dashboard/DashboardPage";
 // import { BookrProvider } from './utils/context';
-import Navbar from './components/navbar/Navbar';
-import { ContextProvider } from './utils/context';
-import { Bookerpallette } from './utils/theme';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Ticket from './components/Drop_Down/Ticket';
-import EventCard from './pages/event-card';
-import TicketSelection from './pages/ticketSelection'
-
-
+import Navbar from "./components/navbar/Navbar";
+import { ContextProvider } from "./utils/context";
+import { Bookerpallette } from "./utils/theme";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Ticket from "./components/Drop_Down/Ticket";
+import EventCard from "./pages/event-card";
+import TicketSelection from "./pages/ticketSelection";
+import EventDetails from "./pages/eventDetails/eventDetails";
 
 const theme = createTheme({
   palette: {
-    ...Bookerpallette
+    ...Bookerpallette,
   },
-})
-
+});
 
 function App() {
   return (
     <>
-    {/* </BookrProvider> */}
+      {/* </BookrProvider> */}
       <ContextProvider>
         <ThemeProvider theme={theme}>
-        <Navbar />  
+          <Navbar />
           <BrowserRouter>
             <Routes>
-              <Route path='' element={<DemoPage />} />
-              <Route path='/dashboard' element={<DashboardPage />} />
-              <Route path='/tickets' element={<Ticket />} />
-              <Route path='/event-card' element={<EventCard />} />
-              <Route path='/ticket-selection' element={<TicketSelection />} />
+              <Route path="" element={<DemoPage />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/tickets" element={<Ticket />} />
+              <Route path="/event-card" element={<EventCard />} />
+              <Route path="/ticket-selection" element={<TicketSelection />} />
+              <Route path="/event-details" element={<EventDetails />} />;
             </Routes>
           </BrowserRouter>
           <Footer />
         </ThemeProvider>
       </ContextProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -1,28 +1,36 @@
 import React, { ButtonHTMLAttributes } from "react";
 import styles from "./Buttons.module.css";
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-    styleType?: "primary" | "secondary";
-    content: "string" | React.ReactNode;
-    height: string;
-    width: string;
-    onClick: () => void;
-    type?: "button" | "submit" | "reset";
+  styleType?: "primary" | "secondary";
+  content: "string" | React.ReactNode;
+  height: string;
+  width: string;
+
+  onClick: () => void;
+  type?: "button" | "submit" | "reset";
 }
 const Button: React.FC<Props> = ({
-    content,
-    styleType = "primary",
-    height,
-    width,
-    onClick,
-    type,
-    ...props
+  content,
+  styleType = "primary" || "secondary",
+  height,
+  width,
+
+  onClick,
+  type,
+  ...props
 }) => {
-    const className = styles[styleType];
-    return (
-        <button className={className} onClick={onClick} style={{ height, width }} type={type} {...props}>
-            {" "}
-            {content}
-        </button>
-    );
+  const className = styles[styleType];
+  return (
+    <button
+      className={className}
+      onClick={onClick}
+      style={{ height, width }}
+      type={type}
+      {...props}
+    >
+      {" "}
+      {content}
+    </button>
+  );
 };
 export default Button;
