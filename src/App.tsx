754596cbcda8b645d3b8d3c5b486a3f94f746c09
Ceeh-Jay <chiemekaelumeziem@gmail.com>
2023-02-20 17:49:50 +0100

    change double quote to signle quote on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import "./App.css";
-import Footer from "./components/footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { DemoPage } from "./pages/demo";
-import { DashboardPage } from "./pages/dashboard/DashboardPage";
-import Navbar from "./components/navbar/Navbar";
-import { ContextProvider } from "./utils/context";
-import { Bookerpallette } from "./utils/theme";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import './App.css';
+import Footer from './components/footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { DemoPage } from './pages/demo';
+import { DashboardPage } from './pages/dashboard/DashboardPage';
+import Navbar from './components/navbar/Navbar';
+import { ContextProvider } from './utils/context';
+import { Bookerpallette } from './utils/theme';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 // import Header from './components/header/Header';
-import MiniHeader from "./components/miniHeader/Miniheader";
+import MiniHeader from './components/miniHeader/Miniheader';
 
-import Ticket from "./components/Drop_Down/Ticket";
-import EventCard from "./pages/event-card";
-import Publish from "./pages/Publish/Publish";
-import FormInput from "./components/search";
-import TicketSelection from "./pages/ticketSelection";
-import HomePageHeader from "./components/homepageHeader/HomePageHeader";
-import { EventPage } from "./pages/ eventPage";
+import Ticket from './components/Drop_Down/Ticket';
+import EventCard from './pages/event-card';
+import Publish from './pages/Publish/Publish';
+import FormInput from './components/search';
+import TicketSelection from './pages/ticketSelection';
+import HomePageHeader from './components/homepageHeader/HomePageHeader';
+import { EventPage } from './pages/ eventPage';
 
-import PaymentInfo from "./pages/paymentInformation";
-import AccountInfo from "./pages/accountInformation";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import Allevent from "./pages/AllEvents/AllEvent";
-import EventDetails from "./pages/eventDetails/eventDetails";
+import PaymentInfo from './pages/paymentInformation';
+import AccountInfo from './pages/accountInformation';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Allevent from './pages/AllEvents/AllEvent';
+import EventDetails from './pages/eventDetails/eventDetails';
 
 const theme = createTheme({
   palette: {
@@ -44,26 +44,26 @@ function App() {
             <HomePageHeader />
             <Routes>
               {/* <Route path='' element={<DemoPage />} /> */}
-              <Route path="/publish" element={<Publish />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/event-card" element={<EventCard />} />
-              <Route path="/event-page" element={<EventPage />} />
-              <Route path="" element={<DemoPage />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/payment-information" element={<PaymentInfo />} />
-              <Route path="/account-information" element={<AccountInfo />} />
-              <Route path="/search" element={<FormInput />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="" element={<DemoPage />} />
-              <Route path="/ticket-selection" element={<TicketSelection />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/events" element={<Allevent />} />
-              <Route path="" element={<DemoPage />} />
-              <Route path="/search" element={<FormInput />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/events" element={<Allevent />} />
-              <Route path="/event-details" element={<EventDetails />} />;
+              <Route path='/publish' element={<Publish />} />
+              <Route path='/dashboard' element={<DashboardPage />} />
+              <Route path='/event-card' element={<EventCard />} />
+              <Route path='/event-page' element={<EventPage />} />
+              <Route path='' element={<DemoPage />} />
+              <Route path='/dashboard' element={<DashboardPage />} />
+              <Route path='/payment-information' element={<PaymentInfo />} />
+              <Route path='/account-information' element={<AccountInfo />} />
+              <Route path='/search' element={<FormInput />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='' element={<DemoPage />} />
+              <Route path='/ticket-selection' element={<TicketSelection />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/events' element={<Allevent />} />
+              <Route path='' element={<DemoPage />} />
+              <Route path='/search' element={<FormInput />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/dashboard' element={<DashboardPage />} />
+              <Route path='/events' element={<Allevent />} />
+              <Route path='/event-details' element={<EventDetails />} />;
             </Routes>
           </BrowserRouter>
           <Footer />
