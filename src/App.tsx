a2318c34fa94f0e275925b1e571185b37e98179a
Deyems <adeyemisola11@gmail.com>
2023-02-20 20:02:32 +0100

    match dev branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Footer from './components/footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { DemoPage } from './pages/demo';
+// import { DemoPage } from './pages/demo'
 import { DashboardPage } from './pages/dashboard/DashboardPage';
 import Navbar from './components/navbar/Navbar';
 import { ContextProvider } from './utils/context';
@@ -10,7 +10,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 // import Header from './components/header/Header';
 import MiniHeader from './components/miniHeader/Miniheader';
 
-import Ticket from './components/Drop_Down/Ticket';
+import Ticket from './components/Drop_Down/Ticket'
 import EventCard from './pages/event-card';
 import Publish from './pages/Publish/Publish';
 import FormInput from './components/search';
@@ -22,7 +22,7 @@ import PaymentInfo from './pages/paymentInformation';
 import AccountInfo from './pages/accountInformation';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-import Allevent from './pages/AllEvents/AllEvent';
+import Allevent from './pages/AllEvents/AllEvent'
 import EventDetails from './pages/eventDetails/eventDetails';
 
 const theme = createTheme({
@@ -45,20 +45,12 @@ function App() {
             <Routes>
               {/* <Route path='' element={<DemoPage />} /> */}
               <Route path='/publish' element={<Publish />} />
-              <Route path='/dashboard' element={<DashboardPage />} />
               <Route path='/event-card' element={<EventCard />} />
               <Route path='/event-page' element={<EventPage />} />
-              <Route path='' element={<DemoPage />} />
-              <Route path='/dashboard' element={<DashboardPage />} />
               <Route path='/payment-information' element={<PaymentInfo />} />
               <Route path='/account-information' element={<AccountInfo />} />
-              <Route path='/search' element={<FormInput />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='' element={<DemoPage />} />
               <Route path='/ticket-selection' element={<TicketSelection />} />
               <Route path='/login' element={<Login />} />
-              <Route path='/events' element={<Allevent />} />
-              <Route path='' element={<DemoPage />} />
               <Route path='/search' element={<FormInput />} />
               <Route path='/register' element={<Register />} />
               <Route path='/dashboard' element={<DashboardPage />} />
