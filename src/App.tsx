3f9bac936354f3924da169cee90eb38831e8dd9e
PELUMIJOY <faslusky@gmail.com>
2023-03-15 14:48:24 +0100

    Add boarder to button in forget and reset password page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css'
 import Footer from './components/footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -14,25 +13,6 @@ import EventCard from './pages/event-card';
 import TicketSelection from './pages/ticketSelection'
 import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
 import ResetPassword from './pages/Reset Password/resetPassword';
-
-=======
-import "./App.css";
-import Footer from "./components/footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { DemoPage } from "./pages/demo";
-import { DashboardPage } from "./pages/dashboard/DashboardPage";
-import Navbar from "./components/navbar/Navbar";
-import { ContextProvider } from "./utils/context";
-import { Bookerpallette } from "./utils/theme";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Ticket from "./components/Drop_Down/Ticket";
-import EventCard from "./pages/event-card";
-import Publish from "./pages/Publish/Publish";
-import FormInput from "./components/search";
-import TicketSelection from "./pages/ticketSelection";
-import { EventPage } from "./pages/ eventPage";
->>>>>>> cdc433107f14952b759bf79dac825d1c3d376a7a
-
 import PaymentInfo from "./pages/paymentInformation";
 import AccountInfo from "./pages/accountInformation";
 import Login from "./pages/login/Login";
@@ -41,6 +21,9 @@ import Allevent from "./pages/AllEvents/AllEvent";
 import EventDetails from "./pages/eventDetails/eventDetails";
 import { Hero } from "./pages/eventPage/Hero";
 import PrivateRoute from "./context/PrivateRoutes";
+import Publish from "./pages/Publish/Publish";
+import FormInput from "./components/search";
+import { EventPage } from "./pages/ eventPage";
 
 const theme = createTheme({
   palette: {
@@ -57,16 +40,6 @@ function App() {
           <Navbar />
           <BrowserRouter>
             <Routes>
-<<<<<<< HEAD
-            {/* <Route path='/nav' element={<Navbar />} /> */}
-              <Route path='' element={<DemoPage />} />
-              <Route path='/dashboard' element={<DashboardPage />} />
-              <Route path='/tickets' element={<Ticket />} />
-              <Route path='/event-card' element={<EventCard />} />
-              <Route path='/ticket-selection' element={<TicketSelection />} />
-              <Route path='/forgot-password' element={<ForgotPassword />} />
-              <Route path='/reset-password' element={<ResetPassword />} />
-=======
               {/* Protected routes start */}
               <Route path="/dashboard" element={<PrivateRoute />}>
                 <Route path="event-page" element={<EventPage />} />
@@ -77,7 +50,6 @@ function App() {
                 <Route path="event-details" element={<EventDetails />} />;
               </Route>
               {/* end */}
-
               <Route path="/publish" element={<Publish />} />
               <Route path="" element={<DemoPage />} />
               <Route path="/search" element={<FormInput />} />
@@ -86,7 +58,8 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/events" element={<Allevent />} />
               <Route path="event-pages" element={<Hero />} />
->>>>>>> cdc433107f14952b759bf79dac825d1c3d376a7a
+              <Route path="forgot-password" element={<ForgotPassword />} />
+              <Route path="reset-password" element={< ResetPassword />} />
             </Routes>
           </BrowserRouter>
           <Footer />
